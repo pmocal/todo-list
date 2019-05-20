4465bc4d18a38edb4ee57472af12aceaac009c8d
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-05-20 15:05:34 -0400

    https://github.com/pmocal/todo-list/issues/1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { addTodoAppLogic, addNotebookAppLogic } from './todo-applogic.js';
 import { attachTodoCreationForm, attachNotebookCreationForm, addTodoDOM, addNotebookDOM } from './todo-dom.js';
 
-attachTodoCreationForm();
+attachTodoCreationForm("default");
 attachNotebookCreationForm();
 
-document.getElementById("default-submit-todo").addEventListener("click", function(){
-	addTodoDOM(JSON.stringify(addTodoAppLogic(document.getElementById("default-duedate").value,
-		document.getElementById("default-title").value,
-		document.getElementById("default-priority").value)));
+document.getElementById("submit-todo").addEventListener("click", function(){
+	addTodoDOM(JSON.stringify(addTodoAppLogic(document.getElementById("duedate").value,
+		document.getElementById("title").value,
+		document.getElementById("priority").value)), document.getElementById("notebook").value);
 });
 
 document.getElementById("submit-notebook").addEventListener("click", function(){
--- src/todo-dom.js
+++ src/todo-dom.js
@@ -1,7 +1,8 @@
-function attachTodoCreationForm() {
-	document.getElementById("default-new-todo").addEventListener("click",
+function attachTodoCreationForm(string) {
+	document.getElementById("new-todo").addEventListener("click",
 		function(){
-			document.getElementById("default-todo-form").style.display = "block";
+			document.getElementById("todo-form").style.display = "block";
+			document.getElementById("notebook-form").style.display = "none";
 		}
 	);
 }
@@ -9,14 +10,15 @@ function attachTodoCreationForm() {
 function attachNotebookCreationForm() {
 	document.getElementById("new-notebook").addEventListener("click",
 		function(){
+			document.getElementById("todo-form").style.display = "none";
 			document.getElementById("notebook-form").style.display = "block";
 		}
 	);
 }
 
-function addTodoDOM(string) {
-	document.getElementById("default-todo-form").style.display = "none";
-	document.getElementById("default-todo-list").innerHTML += "<p>" + string + "</p>";
+function addTodoDOM(string, notebook) {
+	document.getElementById("todo-form").style.display = "none";
+	document.getElementById(notebook + "-todo-list").innerHTML += "<p>" + string + "</p>";
 }
 
 function addNotebookDOM(string) {
@@ -24,7 +26,7 @@ function addNotebookDOM(string) {
 	var element = document.createElement("div");
 	element.id = string + "-todo-list";
 	element.innerHTML = "<h2>" + string + "</h2>";
-	document.body.appendChild(element);
+	document.body.insertBefore(element, document.getElementsByTagName('button')[0]);
 }
 
 export { attachTodoCreationForm, attachNotebookCreationForm, addNotebookDOM, addTodoDOM }
